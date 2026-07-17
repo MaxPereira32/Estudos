ab39387e263c49322202c00174838666a38fea8d
Max <lindomaxpereira@gmail.com>
2026-07-16 23:21:22 -0300

    Commit inicial do projeto Estudos

---
+++ desafio.js
@@ -0,0 +1,23 @@
+let niveis = [
+    { limite: 10, nome: "Ferro" },
+    { limite: 20, nome: "Bronze" },
+    { limite: 50, nome: "Prata" },
+    { limite: 80, nome: "Ouro" },
+    { limite: 90, nome: "Diamante" },
+    { limite: 100, nome: "Lendário" },
+];
+
+let saldoVitorias = 0;
+let nivel = "";
+
+while (nivel !== "Imortal") {
+    let vitorias = 15;
+    let derrotas = 2;
+
+    saldoVitorias += vitorias - derrotas;
+    nivel = niveis.find((n) => saldoVitorias <= n.limite)?.nome ?? "Imortal";
+
+    console.log(`+${vitorias} vitórias / -${derrotas} derrotas | Saldo: ${saldoVitorias} | Nível: ${nivel}`);
+}
+
+console.log(`\nO Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
