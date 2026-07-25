30f761d60e465b73897a365590951e19303ea9a2
Max <lindomaxpereira@gmail.com>
2026-07-25 14:30:52 -0300

    Corrigindo bug de desestruturação e nome da variável em introducao.js

---
+++ Logica/desafio_Ranckeado/trabalhos/Json/introducao.js
@@ -0,0 +1,25 @@
+
+let invoice = {
+    name: "Max", 
+    age: 28, 
+    products: {
+        0: ["mouse 2xwm", 29.90],
+        1: ["Teclado mecânico", 129.99],
+        2: ["Monitor", 899.99]
+    },
+    taxes: "98.90"
+}
+     
+ generateInvoice(invoice)
+     
+ function generateInvoice(invoice){
+ console.log(`O comprador é ${invoice.name}`) 
+ console.log(`A idade é ${invoice.age}`)
+ console.log("------------------" )
+
+ for(let index in invoice.products){ 
+        let [productName, productPrice] = invoice.products[index]
+        console.log(`- ${productName}: R$ ${productPrice}`)
+    }
+ 
+}
